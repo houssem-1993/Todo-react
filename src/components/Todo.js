8fadc9036dd1c0eb16d784ab0b68e0d804410d6e
houssem-1993 <houssem.eddine.ammari@outlook.fr>
2020-04-01 13:57:52 +0200

    first commit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import{connect} from 'react-redux'
+import {addTask,deleteTask,completeTask,editTask,toggleEdit} from '../action/actions'
 
  class Todo extends Component {
      state={
          userInput:'',
          list:[{todo:'my first task',isComplete:true,id:0},{todo:'my second task',isComplete:false,id:1}],
+         editInput:'',
      }
     
     //  change(key,value){
@@ -12,8 +15,19 @@ import React, { Component } from 'react'
     //      })
     //  }
      change =(e)=>{
-         this.setState({userInput:e.target.value})
+         this.setState({[e.target.name]:e.target.value})
      }
+     
+     editTodo =(id) =>{
+         this.props.editTask({
+             id,
+             isComplete:false,
+             todo:this.state.editInput,
+             isEdited:false
+             
+         })
+     }
+     
      
      addTodo= () =>{
         //  create a new todo
@@ -21,8 +35,10 @@ import React, { Component } from 'react'
          const newTodo={
              id:1 +Math.random(),
              todo: this.state.userInput.trim(),
-             isComplete:false
+             isComplete:false,
+             isEdited:false
          };
+         this.props.addTask(newTodo)
          // copy of current list
          const list =[...this.state.list];
          //add a new todo to the list
@@ -37,40 +53,47 @@ import React, { Component } from 'react'
          
         }
     complete =(id) =>{
-        this.setState({
-            list:this.state.list.map(el =>el.id === id?{...el,isComplete:!el.isComplete}:el)
+        // this.setState({
+        //     list:this.state.list.map(el =>el.id === id?{...el,isComplete:!el.isComplete}:el)
+        this.props.completeTask(id)
             
-        })
+        // })
     }
     
          
     deleteTodo = (id) =>{
-        //copy of current list
-        const list=[...this.state.list];
-        //filter out todo being deleted
-        const updateList =list.filter(el => el.id !== id)
-        this.setState({list:updateList});
+        // //copy of current list
+        // const list=[...this.state.list];
+        // //filter out todo being deleted
+        // const updateList =list.filter(el => el.id !== id)
+        // this.setState({list:updateList});
+        this.props.deleteTask(id)
     }
+    
         
         
     render() {
+        console.log(this.props.todo)
         return (
         <div className="head">
             <div className="first">
                 <h1>To Do App</h1>
                 <label>Add new To-Do</label>
-                <input className="input1" type="text" placeholder="Enter new task" value={this.state.userInput} onChange={this.change}/>
+                <input className="input1" type="text" name="userInput" placeholder="Enter new task" value={this.state.userInput} onChange={this.change}/>
                 <input type="button" className="add"  onClick={() => this.addTodo()} value="Add!"/>
 
             </div>
             <div className="my-todo-list">
                 
-                {this.state.list.map((el, i) => (
+                {this.props.todo.map((el, i) => (
                     <div className="my-todo" key={el.id}>
                    
                 <button className="complete-btn" onClick={()=>this.complete(el.id)}>{el.isComplete?"undo":"Complete"}</button>
                     <button className="delete-btn" onClick={() => this.deleteTodo(el.id)}>delete</button>
+                <button className="edit-btn" onClick={() => el.isEdited? this.editTodo(el.id):this.props.toggleEdit(el.id)}>{el.isEdited?"comfirm":"edit"}</button> 
+                    {el.isEdited&&<input type="text" className="edit-input" name="editInput" value={this.state.editInput} onChange={this.change} />}
                     <p className={el.isComplete?"todo-complete":''}>{el.todo}</p>
+
                 </div>
                  
                 )
@@ -83,4 +106,16 @@ import React, { Component } from 'react'
         )
     }
  }
-export default Todo
+ //redux
+ const mapStateToProps=state=>{
+     return {todo:state}
+ }
+ const mapDispatchToProps =dispatch =>{
+     return{ addTask:(payload)=>dispatch(addTask(payload)),
+        deleteTask:(payload)=>dispatch(deleteTask(payload)),
+        completeTask:(payload)=>dispatch(completeTask(payload)),
+        editTask:(payload)=>dispatch(editTask(payload)),
+        toggleEdit:(payload)=>dispatch(toggleEdit(payload)),
+        }
+ }
+export default connect(mapStateToProps,mapDispatchToProps) (Todo)
